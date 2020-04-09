84a66769991ef5dbd8bc98abc5a0249978ebadd6
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2020-04-08 23:35:35 -0400

    testing with modules

--- public/felicity.js
+++ public/felicity.js
@@ -1,13 +1,13 @@
 const Felicity = {
-    talking:false,
-    voices:window.speechSynthesis.getVoices(),
-    voice:4,
-    say:function(message){
-      var msg = new SpeechSynthesisUtterance(message);
-      msg.voice = this.voices[voice];
-      msg.onend = function(e) {
-        console.log('Finished in ' + event.elapsedTime + ' seconds.');
-      };
-      window.speechSynthesis.speak(msg);
-    }
+  talking:false,
+  voices:window.speechSynthesis.getVoices(),
+  voice:4,
+  say:function(message){
+    var msg = new SpeechSynthesisUtterance(message);
+    msg.voice = this.voices[this.voice];
+    msg.onend = function(e) {
+      console.log('Finished in ' + event.elapsedTime + ' seconds.');
+    };
+    window.speechSynthesis.speak(msg);
   }
+}
