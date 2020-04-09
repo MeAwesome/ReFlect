a00fef2e7d5690f11bccd74c4852d3be480f7744
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2020-04-08 23:09:23 -0400

    Update felicity.js

--- public/felicity.js
+++ public/felicity.js
@@ -1,21 +1,14 @@
-var Felicity;
-window.onload = function(){
-  Felicity = {
-    talking:false,
-    utterance:new SpeechSynthesisUtterance(),
-    settings:{
-      lang:"en-US",
-      voice:window.speechSynthesis.getVoices()[4]
-    },
-    say:function(message){
-      var msg = this.utterance;
-      msg.text = message;
-      msg.lang = this.settings.lang;
-      msg.voice = this.settings.voice;
-      msg.onend = function(e) {
-        console.log('Finished in ' + event.elapsedTime + ' seconds.');
-      };
-      window.speechSynthesis.speak(msg);
-    }
+const Felicity = {
+  talking:false,
+  utterance:new SpeechSynthesisUtterance(),
+  voice:window.speechSynthesis.getVoices()[4],
+  say:function(message){
+    var msg = this.utterance;
+    msg.text = message;
+    msg.voice = this.voice;
+    msg.onend = function(e) {
+      console.log('Finished in ' + event.elapsedTime + ' seconds.');
+    };
+    window.speechSynthesis.speak(msg);
   }
 }
