eca77a0c3842c59d36c90a37e07d2dfac587c074
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2020-06-06 01:09:45 -0400

    Update mirror.js

--- public/js/mirror.js
+++ public/js/mirror.js
@@ -10,12 +10,25 @@ function refreshMirror(){
   mirrorDisplay.copyData(mirror, 0, 0, mirrorDisplay.canvas.width, mirrorDisplay.canvas.height);
   window.requestAnimationFrame(refreshMirror);
 }
+
+function weather(){
+  Melody.say("I don't have the information for the weather yet.");
+}
+
+function hi(){
+  Melody.say("Hello! What can I do for you?", true);
+}
+
+window.addEventListener("melodyLoaded", (e) => {
+	Melody.addCommand("wake", ["hello"], hi);
+  Melody.addCommand("weather", ["weather", "today"], weather);
+});
 /*
 window.addEventListener("melodyHeard", (e) => {
   if(Melody.lastHeard.containsOne(["hi", "hello", "hey", "what's up", "high", "hay"])){
-    Melody.say("Hello! What can I do for you?", true);
+
   } else if(Melody.lastHeard.containsAll(["weather", "today"])){
-    Melody.say("I don't have the information for the weather yet.");
+
   }
 });
 */
