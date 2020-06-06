ac0d72115b43ac140e7a9b181031d992775cd8df
Isaac Robbins <34529363+MeAwesome@users.noreply.github.com>
2020-06-06 01:14:10 -0400

    Revert "turn off responsive voice console spam"
    This reverts commit 511ee6f76e18a42d521314b4c38646aeca0a8204.

--- public/js/melody.js
+++ public/js/melody.js
@@ -104,7 +104,6 @@ const Melody = {
 if(window.speechSynthesis.getVoices().length == 0){
   Melody.voices = responsiveVoice.getVoices();
   Melody.fallbackMode = true;
-  responsiveVoice.debug = false;
 } else {
   Melody.voices = window.speechSynthesis.getVoices();
   Melody.fallbackMode = false;
